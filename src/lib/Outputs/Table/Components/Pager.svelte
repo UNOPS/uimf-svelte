70cbb0669d58f0d430ca1c52f9b2f16c4533a029
JeanLaurent <jlcassier@gmail.com>
2023-09-12 16:40:41 +0700

    feat(Pager): keep input and pagination parameters when refreshing component

--- src/lib/Outputs/Table/Components/Pager.svelte
+++ src/lib/Outputs/Table/Components/Pager.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <script lang="ts">
+	import { OutputComponentController } from '../../../Infrastructure/ComponentController';
 	import type { PaginatedData } from '$lib/Outputs/PaginatedData.svelte';
+	import { beforeUpdate } from 'svelte';
 	export let controller: OutputController<PaginatedData>;
 
 	let paginator =
@@ -12,11 +14,72 @@
 
 	let pageSize = paginator.value.PageSize ?? pageSizes[0];
 	let pageCount = controller.value.TotalCount / pageSize + 1;
-	let ActivePageIndex = paginator.value.PageIndex ?? 1;
+	let activePageIndex = paginator.value.PageIndex ?? 1;
 	let maxPage: number = Math.floor(controller.value.TotalCount / pageSize + 1);
 
+	let inputValues: any;
+
+	// array of urls
+	let pageUrls: string[] = new Array(maxPage);
+	let selectorUrls: string[] = new Array(pageSizes.length);
+
+	let component = new OutputComponentController({
+		async init() {
+			inputValues = await controller.form?.getInputFieldValues();
+			setCountPage();
+			LoadPageUrls();
+			LoadSelectorUrls();
+		},
+
+		async refresh() {
+			await controller.form?.getInputFieldValues().then(async (results) => {
+				if (inputValues != undefined && !areEqual(inputValues, results)) {
+					activePageIndex = 1;
+				}
+
+				inputValues = results;
+				setCountPage();
+				LoadSelectorUrls();
+				LoadPageUrls();
+
+				controller.value = controller.value;
+			});
+		}
+	});
+
+	beforeUpdate(async () => await component.setup(controller));
+
+	let LoadSelectorUrls = async () => {
+		pageSizes.forEach(async (element, i) => {
+			selectorUrls[i] = await getUrl(activePageIndex, element);
+		});
+	};
+
+	let LoadPageUrls = async () => {
+		for (let i = 0; i < maxPage; i++) {
+			pageUrls[i] = await getUrl(i + 1, pageSize);
+		}
+	};
+
+	function areEqual(obj1: any, obj2: any): boolean {
+		for (let key in obj1) {
+			if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+				if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+					if (!areEqual(obj1[key], obj2[key])) {
+						return false;
+					}
+				} else if (obj1[key] !== obj2[key]) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	const getInputFieldValuesForPage = async (index: number, size: number) => {
-		const inputValues = await controller.form?.getInputFieldValues();
+		//const inputValues = await controller.form?.getInputFieldValues();
 
 		return {
 			...inputValues,
@@ -29,34 +92,38 @@
 
 	const getUrl = async (index: number, size: number) => {
 		const inputFieldValues = await getInputFieldValuesForPage(index, size);
-	
+
 		return controller.app.makeUrl({
 			Form: controller.form!.metadata.Id,
 			InputFieldValues: inputFieldValues
 		});
 	};
+
+	function setCountPage() {
+
+		maxPage = Math.floor(controller.value.TotalCount / pageSize + 1);
+		console.log("max page", maxPage);
+}
 </script>
 
 {#if pageCount >= 1}
 	<nav class="pagination">
 		<div class="per-page-selector">
-			{#each pageSizes as size, index}
-				{#await getUrl(ActivePageIndex, size) then url}
-					<a href={url}> {size} </a>
-					{#if index + 1 != pageSizes.length}
-						<span> - </span>
-					{/if}
-				{/await}
+			{#each selectorUrls as url, index}
+				<a href={url}>{pageSizes[index]}</a>
+
+				<!-- no space after last page -->
+				{#if index != pageSizes.length - 1}
+					<span> - </span>
+				{/if}
 			{/each}
 		</div>
 		<div class="page-selector">
 			<ul>
-				{#each Array(maxPage) as _, index (index + 1)}
-					{#await getUrl(index + 1, pageSize) then url}
-						<li class={ActivePageIndex === index + 1 ? 'active' : ''}>
-							<a href={url} on:click={() => (ActivePageIndex = index + 1)}>{index + 1}</a>
-						</li>
-					{/await}
+				{#each pageUrls as url, index (index + 1)}
+					<li class={activePageIndex === index + 1 ? 'active' : ''}>
+						<a href={url} on:click={() => (activePageIndex = index + 1)}>{index + 1}</a>
+					</li>
 				{/each}
 			</ul>
 		</div>
