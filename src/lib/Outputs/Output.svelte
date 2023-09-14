b3ceeb48d80a1e9167c2769cc9090222fa260c14
JeanLaurent <jlcassier@gmail.com>
2023-09-14 12:06:49 +0700

    cleanup

--- src/lib/Outputs/Output.svelte
+++ src/lib/Outputs/Output.svelte
@@ -25,33 +25,32 @@
 
 	beforeUpdate(async () => await component.setup(controller));
 
-	
 	let componentController: ComponentController;
 
 	function getComponentController(nestedMetadata: ComponentMetadata): any {
 		let componentController: ComponentController = {
-					component: controlRegister.outputs[nestedMetadata.Type].component,
-					controller: new OutputController<any>({
-						metadata: nestedMetadata,
-						data: null,
-						form: controller.form!,
-						app: controller.app
-					})
-				};
-				
-				componentController.controller.setValue(controller.value?.Value);
-				
+			component: controlRegister.outputs[nestedMetadata.Type].component,
+			controller: new OutputController<any>({
+				metadata: nestedMetadata,
+				data: null,
+				form: controller.form!,
+				app: controller.app
+			})
+		};
+
+		componentController.controller.setValue(controller.value?.Value);
+
 		return componentController;
 	}
 </script>
 
 {#if componentController != null}
 	<div class={controller.metadata.CustomProperties?.cssClass}>
-			<div class={componentController.controller.metadata.CustomProperties?.cssClass}>
-				<svelte:component
-					this={componentController.component}
-					controller={componentController.controller}
-				/>
-			</div>
+		<div class={componentController.controller.metadata.CustomProperties?.cssClass}>
+			<svelte:component
+				this={componentController.component}
+				controller={componentController.controller}
+			/>
+		</div>
 	</div>
 {/if}
