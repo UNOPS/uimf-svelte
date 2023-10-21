a5095e188ca89ddf65615efd624bdea43fe79be3
Nikita Pshenov <nikitap@unops.org>
2023-10-21 17:32:17 +0700

    docs(ActionListColumnExtension)

--- src/lib/Outputs/Table/Extensions/ActionListColumnExtension.ts
+++ src/lib/Outputs/Table/Extensions/ActionListColumnExtension.ts
@@ -1,6 +1,9 @@
 import type { Table, TableHeadCell } from "..";
 import { TableExtension } from "../TableExtension";
 
+/**
+ * Hides action-list column if none of the rows have any actions in that column.
+ */
 export class ActionListColumnExtension extends TableExtension {
     processHeadCell(table: Table, cell: TableHeadCell, rows: any[]) {
         if (cell.metadata.Type === 'action-list') {
