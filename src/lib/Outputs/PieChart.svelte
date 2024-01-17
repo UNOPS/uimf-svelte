792e4e1a703ef94d01ee197a965d5b53441d6681
Nik P <niaher.crimston@gmail.com>
2024-01-17 14:20:34 +0700

    feat(pie-chart)

---
+++ src/lib/Outputs/PieChart.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { OutputComponent } from '../Infrastructure/Component';
+	import { beforeUpdate } from 'svelte';
+	import type { OutputController } from '../Infrastructure/OutputController';
+
+	interface IPieChart {
+        // TODO: must correspond to the server-side UIMF object that
+        // contains the data for the pie chart.
+    }
+
+	export let controller: OutputController<IPieChart>;
+
+	let component = new OutputComponent({
+		refresh() {
+			controller.value = controller.value;
+		}
+	});
+
+	beforeUpdate(async () => await component.setup(controller));
+</script>
+
+Pie chart component is not implemented yet.
+<!-- TODO: implement html to show the pie chart -->
+
+<style lang="scss">
+	@import '../scss/styles.variables.scss';
+</style>
