fc51268100ee6402de5abf20e08c9649c0939621
Syed Muhammad Ahmed <isyedahmed531@gmail.com>
2024-01-24 12:36:08 +0500

    create(pie-chart): implementation of basic pie-chart functionality

--- src/lib/Outputs/PieChart.svelte
+++ src/lib/Outputs/PieChart.svelte
@@ -2,16 +2,38 @@
 	import { OutputComponent } from '../Infrastructure/Component';
 	import { beforeUpdate } from 'svelte';
 	import type { OutputController } from '../Infrastructure/OutputController';
+	import { onMount } from 'svelte';
+	import { spring, tweened } from 'svelte/motion';
+
+	interface IPieChartData {
+		Label: string;
+		Value: number;
+	}
 
 	interface IPieChart {
-        // TODO: must correspond to the server-side UIMF object that
-        // contains the data for the pie chart.
-    }
+		Data: IPieChartData[];
+	}
+
+	export let size = 200;
+	export let percent = 0;
+	export const bgColor = 'cornflowerblue';
+	export const fgColor = 'orange';
+
+	$: viewBox = `0 0 ${size} ${size}`;
+	$: radius = size / 2;
+	$: halfCircumference = Math.PI * radius;
+	$: pieSize = halfCircumference * (percent / 100);
+	$: dashArray = `0 ${halfCircumference - pieSize} ${pieSize}`;
+
+	const store = tweened(0, { duration: 1000 });
+	$: store.set(percent);
 
 	export let controller: OutputController<IPieChart>;
 
 	let component = new OutputComponent({
 		refresh() {
+			//const data = controller.value.Data;
+
 			controller.value = controller.value;
 		}
 	});
@@ -19,9 +41,32 @@
 	beforeUpdate(async () => await component.setup(controller));
 </script>
 
-Pie chart component is not implemented yet.
-<!-- TODO: implement html to show the pie chart -->
+<div>
+	<!-- not being displayed -->
+	<h2>Check if working</h2>
+</div>
+<!-- For checking purpose -->
+<label>
+	Percent
+	<input type="number" min="0" max="100" bind:value={percent} />
+</label>
+
+<svg width={size} height={size} {viewBox}>
+	<circle r={radius} cx={radius} cy={radius} fill={bgColor} />
+	<circle
+		r={radius / 2}
+		cx={radius}
+		cy={radius}
+		fill={bgColor}
+		stroke={fgColor}
+		stroke-width={radius}
+		stroke-dasharray={dashArray}
+	/>
+</svg>
 
 <style lang="scss">
-	@import '../scss/styles.variables.scss';
+	// @import '../scss/styles.variables.scss';
+	.div {
+		display: flex;
+	}
 </style>
