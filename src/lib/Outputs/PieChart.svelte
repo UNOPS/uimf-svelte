3a2d973c16e27a26ac30dfb88bbeffed4b53fe11
Syed Muhammad Ahmed <isyedahmed531@gmail.com>
2024-01-25 02:29:12 +0500

    feature(piechart.svelte): implement piechart to visualize supplier portion

--- src/lib/Outputs/PieChart.svelte
+++ src/lib/Outputs/PieChart.svelte
@@ -2,71 +2,102 @@
 	import { OutputComponent } from '../Infrastructure/Component';
 	import { beforeUpdate } from 'svelte';
 	import type { OutputController } from '../Infrastructure/OutputController';
-	import { onMount } from 'svelte';
-	import { spring, tweened } from 'svelte/motion';
 
 	interface IPieChartData {
+		Color: string;
 		Label: string;
 		Value: number;
+		StartAngle: number;
+		EndAngle: number;
 	}
 
 	interface IPieChart {
 		Data: IPieChartData[];
 	}
 
-	export let size = 200;
-	export let percent = 0;
-	export const bgColor = 'cornflowerblue';
-	export const fgColor = 'orange';
-
-	$: viewBox = `0 0 ${size} ${size}`;
-	$: radius = size / 2;
-	$: halfCircumference = Math.PI * radius;
-	$: pieSize = halfCircumference * (percent / 100);
-	$: dashArray = `0 ${halfCircumference - pieSize} ${pieSize}`;
-
-	const store = tweened(0, { duration: 1000 });
-	$: store.set(percent);
-
 	export let controller: OutputController<IPieChart>;
 
 	let component = new OutputComponent({
 		refresh() {
-			//const data = controller.value.Data;
-
+			const data = controller.value.Data;
+			console.log(data);
 			controller.value = controller.value;
 		}
 	});
-
 	beforeUpdate(async () => await component.setup(controller));
+
+	$: totalValue = controller.value.Data.reduce((total, data) => total + data.Value, 0);
+	$: controller.value.Data.forEach((data) => {
+		data.Value = (data.Value / totalValue) * 100;
+	});
+
+	export let size = 200;
+	let accumulatedValue = 0;
+	// Calculate the start and end angles for each data item
+	$: controller.value.Data.forEach((data, i) => {
+		data.StartAngle = (2 * Math.PI * accumulatedValue) / 100;
+		data.EndAngle = (2 * Math.PI * (accumulatedValue + data.Value)) / 100;
+		accumulatedValue += data.Value;
+	});
+	$: controller.value.Data.forEach((data, i) => {
+		data.Color = controller.app.colorFromString(data.Label, {
+			format: 'cmyk',
+			alpha: 0.5,
+			luminosity: 'dark'
+		});
+	});
 </script>
 
-<div>
-	<!-- not being displayed -->
-	<h2>Check if working</h2>
-</div>
-<!-- For checking purpose -->
-<label>
-	Percent
-	<input type="number" min="0" max="100" bind:value={percent} />
-</label>
-
-<svg width={size} height={size} {viewBox}>
-	<circle r={radius} cx={radius} cy={radius} fill={bgColor} />
-	<circle
-		r={radius / 2}
-		cx={radius}
-		cy={radius}
-		fill={bgColor}
-		stroke={fgColor}
-		stroke-width={radius}
-		stroke-dasharray={dashArray}
-	/>
-</svg>
+{#if controller.value.Data != null}
+	<div class="container">
+		<div class="piechart">
+			<svg width={size} height={size}>
+				{#each controller.value.Data as data (data.Label)}
+					<path
+						d="M{size / 2},{size / 2} L{size / 2 + (size / 2) * Math.cos(data.StartAngle)} {size /
+							2 +
+							(size / 2) * Math.sin(data.StartAngle)} A{size / 2},{size / 2} 0 {data.Value > 50
+							? 1
+							: 0},1 {size / 2 + (size / 2) * Math.cos(data.EndAngle)} {size / 2 +
+							(size / 2) * Math.sin(data.EndAngle)} Z"
+						fill={data.Color}
+					/>
+				{/each}
+			</svg>
+		</div>
+		<div class="piechart-legend">
+			<ul>
+				{#each controller.value.Data as data (data.Label)}
+					<ol>
+						<span style="background: {data.Color};" />
+						{data.Label}: {data.Value}%
+					</ol>
+				{/each}
+			</ul>
+		</div>
+	</div>
+{/if}
 
 <style lang="scss">
-	// @import '../scss/styles.variables.scss';
-	.div {
+	.container {
 		display: flex;
+		justify-content: center;
+	}
+	.piechart,
+	.piechart-legend {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.piechart-legend > ul > ol > span {
+		display: inline-block;
+		width: 1em;
+		height: 1em;
+	}
+	.piechart-legend > ul > ol > span {
+		display: inline-block;
+		width: 1em;
+		height: 1em;
 	}
 </style>
