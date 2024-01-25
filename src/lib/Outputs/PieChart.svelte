e08f61991f304ee4676d4aea82e1031c785fbf83
Syed Muhammad Ahmed <isyedahmed531@gmail.com>
2024-01-25 21:08:28 +0500

    fix(piechart.svelte): performance improvement

--- src/lib/Outputs/PieChart.svelte
+++ src/lib/Outputs/PieChart.svelte
@@ -18,24 +18,24 @@
 		Data: IPieChartDisplayData[];
 	}
 
-	export let size = 200; //size for PieChart
 	export let controller: OutputController<IPieChart>;
-	export let totalValue = 0;
+	export let items: IPieChartDisplayData[] = [];
+
+	let size = 200; //size for PieChart
+	let totalValue = 0;
 
 	let component = new OutputComponent({
 		refresh() {
-			const data = controller.value.Data;
-			console.log(data);
-
 			if (controller.value.Data) {
+				items = controller.value.Data;
 				let accumulatedValue = 0;
-				totalValue = controller.value.Data.reduce((total, data) => total + data.Value, 0);
-				controller.value.Data.forEach((data, i) => {
-					data.Value = (data.Value / totalValue) * 100;
-					data.StartAngle = (2 * Math.PI * accumulatedValue) / 100;
-					data.EndAngle = (2 * Math.PI * (accumulatedValue + data.Value)) / 100;
-					accumulatedValue += data.Value;
-					data.Color = controller.app.colorFromString(data.Label, {
+				totalValue = items.reduce((total, item) => total + item.Value, 0);
+				items.forEach((item, i) => {
+					item.Value = (item.Value / totalValue) * 100;
+					item.StartAngle = (2 * Math.PI * accumulatedValue) / 100;
+					item.EndAngle = (2 * Math.PI * (accumulatedValue + item.Value)) / 100;
+					accumulatedValue += item.Value;
+					item.Color = controller.app.colorFromString(item.Label, {
 						format: 'cmyk',
 						alpha: 1,
 						luminosity: 'bright'
@@ -48,20 +48,20 @@
 	beforeUpdate(async () => await component.setup(controller));
 </script>
 
-{#if controller.value.Data != null}
+{#if items != null}
 	<div class="container">
 		<div class="piechart">
 			<svg width={size} height={size}>
-				{#each controller.value.Data as data (data.Label)}
-					{#if typeof data.StartAngle === 'number' && typeof data.EndAngle === 'number'}
+				{#each items as item (item.Label)}
+					{#if typeof item.StartAngle === 'number' && typeof item.EndAngle === 'number'}
 						<path
-							d="M{size / 2},{size / 2} L{size / 2 + (size / 2) * Math.cos(data.StartAngle)} {size /
+							d="M{size / 2},{size / 2} L{size / 2 + (size / 2) * Math.cos(item.StartAngle)} {size /
 								2 +
-								(size / 2) * Math.sin(data.StartAngle)} A{size / 2},{size / 2} 0 {data.Value > 50
+								(size / 2) * Math.sin(item.StartAngle)} A{size / 2},{size / 2} 0 {item.Value > 50
 								? 1
-								: 0},1 {size / 2 + (size / 2) * Math.cos(data.EndAngle)} {size / 2 +
-								(size / 2) * Math.sin(data.EndAngle)} Z"
-							fill={data.Color}
+								: 0},1 {size / 2 + (size / 2) * Math.cos(item.EndAngle)} {size / 2 +
+								(size / 2) * Math.sin(item.EndAngle)} Z"
+							fill={item.Color}
 						/>
 					{:else}
 						<p>Invalid</p>
@@ -71,10 +71,10 @@
 		</div>
 		<div class="piechart-legend">
 			<ul>
-				{#each controller.value.Data as data (data.Label)}
+				{#each items as item (item.Label)}
 					<ol>
-						<span style="background: {data.Color};" />
-						{data.Label}: {data.Value}%
+						<span style="background: {item.Color};" />
+						{item.Label}: {item.Value}%
 					</ol>
 				{/each}
 			</ul>
