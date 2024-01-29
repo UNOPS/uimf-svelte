67bd675d44cc2254b50cde32adf080914dc2a5d3
Nik P <niaher.crimston@gmail.com>
2024-01-29 17:53:57 +0700

    fix(paginated-data): refresh pager

--- src/lib/Outputs/PaginatedData.svelte
+++ src/lib/Outputs/PaginatedData.svelte
@@ -6,11 +6,22 @@
 </script>
 
 <script lang="ts">
+	import { beforeUpdate } from 'svelte';
+
 	import type { OutputController } from '../Infrastructure/OutputController';
 	import Pager from './Table/Components/Pager.svelte';
 	import ResultsTable from './Table/Components/ResultsTable.svelte';
+	import { OutputComponent } from '../Infrastructure/Component';
 
 	export let controller: OutputController<PaginatedData>;
+
+	let component = new OutputComponent({
+		refresh() {
+			controller.value = controller.value;
+		}
+	});
+
+	beforeUpdate(async () => await component.setup(controller));
 </script>
 
 <ResultsTable {controller} type="paginated-data" />
