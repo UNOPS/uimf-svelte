d8713c30d47536b7849361a0923128e3fb667eff
Syed Muhammad Ahmed <isyedahmed531@gmail.com>
2024-02-05 10:34:51 +0500

    create(tidystree.svelte): implementation of tidytree outpout component

---
+++ src/lib/Outputs/tidytree.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { OutputComponent } from '../Infrastructure/Component';
+	import { beforeUpdate } from 'svelte';
+	import type { OutputController } from '../Infrastructure/OutputController';
+	import * as d3 from 'd3';
+
+	interface ITidyTree {
+		Id: number;
+		ParentId: number;
+		Label: string;
+	}
+	interface ITidyTreeData {
+		Data: ITidyTree[];
+	}
+	export let controller: OutputController<ITidyTree>;
+	export let items: ITidyTreeData[] = [];
+
+	let component = new OutputComponent({
+		refresh() {}
+	});
+	beforeUpdate(async () => await component.setup(controller));
+</script>
+
+{#if items != null}{/if}
