f5864aee6e4823f50ecd00decbd99addacdf925f
Syed Muhammad Ahmed <isyedahmed531@gmail.com>
2024-02-21 14:02:30 +0500

    fix: LineChart

--- src/lib/Outputs/LineChart.svelte
+++ src/lib/Outputs/LineChart.svelte
@@ -3,6 +3,7 @@
 	import { beforeUpdate } from 'svelte';
 	import type { OutputController } from '../Infrastructure/OutputController';
 	import * as d3 from 'd3';
+	import { curveBasis, line } from 'd3';
 
 	interface ILineChartData {
 		Label: string;
@@ -12,84 +13,130 @@
 	interface ILineChart {
 		Data: ILineChartData[];
 	}
+	let pathLine = line<ILineChartData>()
+		.x((d) => 0)
+		.y((d) => 0)
+		.curve(curveBasis);
 	let xScale: any;
 	let yScale: any;
-	let chartWidth = 640;
-	let chartHeight = 500;
-	let paddings = {
-		top: 50,
-		left: 50,
-		right: 50,
-		bottom: 50
-	};
-
+	let width = 928;
+	let height = 500;
+	let marginTop = 20;
+	let marginRight = 30;
+	let marginBottom = 30;
+	let marginLeft = 40;
 	let data: ILineChartData[] = [];
+
 	export let controller: OutputController<ILineChart>;
 
 	let component = new OutputComponent({
 		refresh() {
 			if (controller.value) {
 				data = controller.value.Data;
-				console.log('If block running' + data);
-				renderLineChart(controller.value.Data);
+				console.log(data);
+				if (data[1] && data[1].Year instanceof Date) {
+					console.log(data[1].Year.getFullYear());
+				}
+				const xExtent = d3.extent(
+					data.map((d) => {
+						const date = new Date(d.Year);
+						return date.getFullYear();
+					})
+				);
+
+				const yExtent = d3.extent(data.map((d) => d.Value));
+				console.log(data[0].Year, data[0].Value);
+				xScale = d3
+					.scaleLinear()
+					.domain(xExtent[0] === undefined ? [0, 0] : xExtent)
+					.range([5, 95]);
+
+				yScale = d3
+					.scaleLinear()
+					.domain(yExtent[0] === undefined ? [0, 0] : yExtent)
+					.range([5, 75]);
+				// the path generator
+				pathLine = line<ILineChartData>()
+					.x((d) => xScale(+d.Year)) // convert date to timestamp
+					.y((d) => yScale(d.Value))
+					.curve(curveBasis);
 			} else console.log('No value');
 		}
 	});
-
-	function renderLineChart(data: ILineChartData[]) {
-		console.log('renderLineChart function called');
-
-		xScale = d3
-			.scaleTime()
-			.domain(d3.extent(data, (d: { Year: any }) => d.Year))
-			.range([paddings.left, chartWidth - paddings.right]);
-
-		yScale = d3
-			.scaleLinear()
-			.domain([
-				d3.min(data, (d: { Value: any }) => d.Value),
-				d3.max(data, (d: { Value: any }) => d.Value)
-			])
-			.range([chartHeight - paddings.bottom, paddings.top])
-			.nice(10);
-	}
 
 	beforeUpdate(async () => {
 		await component.setup(controller);
 	});
 </script>
 
-<svg width={chartWidth} height={chartHeight}>
-	<g>
-		<line
-			x1={paddings.left}
-			x2={chartWidth - paddings.right}
-			y1={chartHeight - paddings.bottom}
-			y2={chartHeight - paddings.bottom}
-			stroke="black"
-			stroke-width="2"
-		/>
-		<line
-			x1={paddings.left}
-			x2={paddings.left}
-			y1={paddings.top}
-			y2={chartHeight - paddings.bottom}
-			stroke="black"
-			stroke-width="2"
-		/>
-	</g>
-	<g>
-		{#each data as datum, i (datum.Year)}
-			{#if i != data.length - 1}
-				<line
-					x1={xScale(data[i].Year)}
-					x2={xScale(data[i + 1].Year)}
-					y1={yScale(data[i].Value)}
-					y2={yScale(data[i + 1].Value)}
-					stroke="red"
-					stroke-width="2"
-				/>
-			{/if}
-		{/each}
-	</g>
-</svg>
+{#if xScale && yScale}
+	<svg {width} {height} viewBox="0 0 {width} {height}" style:max-width="100%" style:height="auto">
+		<!-- X-Axis -->
+		<g transform="translate(0,{height - marginBottom})">
+			<line stroke="currentColor" x1={marginLeft - 6} x2={width} />
+
+			{#each xScale.ticks() as tick}
+				<!-- X-Axis Ticks -->
+				<line stroke="currentColor" x1={xScale(tick)} x2={xScale(tick)} y1={0} y2={6} />
+
+				<!-- X-Axis Tick Labels -->
+				<text fill="currentColor" text-anchor="middle" x={xScale(tick)} y={22}>
+					{new Date(tick).getFullYear()}
+				</text>
+			{/each}
+		</g>
+
+		<!-- Y-Axis and Grid Lines -->
+		<g transform="translate({marginLeft},0)">
+			{#each yScale.ticks() as tick}
+				{#if tick !== 0}
+					<!-- 
+          Grid Lines. 
+          Note: First line is skipped since the x-axis is already present at 0. 
+        -->
+					<line
+						stroke="currentColor"
+						stroke-opacity="0.1"
+						x1={0}
+						x2={width - marginLeft}
+						y1={yScale(tick)}
+						y2={yScale(tick)}
+					/>
+
+					<!-- 
+          Y-Axis Ticks. 
+          Note: First tick is skipped since the x-axis already acts as a tick. 
+        -->
+					<line stroke="currentColor" x1={0} x2={-6} y1={yScale(tick)} y2={yScale(tick)} />
+				{/if}
+
+				<!-- Y-Axis Tick Labels -->
+				<text
+					fill="currentColor"
+					text-anchor="end"
+					dominant-baseline="middle"
+					x={-9}
+					y={yScale(tick)}
+				>
+					{tick}
+				</text>
+			{/each}
+
+			<!-- Y-Axis Label -->
+			<text fill="currentColor" text-anchor="start" x={-marginLeft} y={15}>
+				↑ Daily close ($)
+			</text>
+		</g>
+
+		<path fill="none" stroke="steelblue" stroke-width="1.5" d={pathLine(data)} />
+	</svg>
+{/if}
+
+<style>
+	path {
+		stroke: purple;
+		stroke-width: 1;
+		fill: none;
+		stroke-linecap: round;
+	}
+</style>
