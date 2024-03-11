c7f1a75405780053a5b38ad48ff4f6cd77299750
Nik P <niaher.crimston@gmail.com>
2024-03-11 15:53:33 +0700

    feat(outputs/number): format number

--- src/lib/Outputs/Number.svelte
+++ src/lib/Outputs/Number.svelte
@@ -1,19 +1,35 @@
+<script lang="ts" context="module">
+	interface Configuration {
+		Formatted?: boolean;
+	}
+</script>
+
 <script lang="ts">
 	import { beforeUpdate } from 'svelte';
 	import type { OutputController } from '../Infrastructure/OutputController';
 	import { OutputComponent } from '../Infrastructure/Component';
+	import type { IFieldMetadata } from '$lib/Infrastructure/uimf';
 
-	export let controller: OutputController<string>;
+	export let controller: OutputController<number, IFieldMetadata<Configuration>>;
+	let displayValue: string | null = null;
 
 	let component = new OutputComponent({
 		refresh() {
-			controller.value = controller.value;
+			if (controller.value != null) {
+				if (controller.metadata.Component?.Configuration?.Formatted) {
+					displayValue = new Intl.NumberFormat().format(controller.value);
+				} else {
+					displayValue = controller.value.toString();
+				}
+			} else {
+				displayValue = null;
+			}
 		}
 	});
 
 	beforeUpdate(async () => await component.setup(controller));
 </script>
 
-{#if controller.value != null}
-	{controller.value}
+{#if displayValue != null}
+	{displayValue}
 {/if}
