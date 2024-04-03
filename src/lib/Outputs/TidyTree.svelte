60630a7045ffdea757d176b8b56e6302dc50661a
Nik P <niaher.crimston@gmail.com>
2024-04-03 13:08:20 +0700

    fix(tidy-tree): compact, tidy look

--- src/lib/Outputs/TidyTree.svelte
+++ src/lib/Outputs/TidyTree.svelte
@@ -4,25 +4,30 @@
 	import type { OutputController } from '../Infrastructure/OutputController';
 	import * as d3 from 'd3';
 
-	interface ITidyTreeData {
+	interface ITree {
+		Nodes: INode[];
+	}
+
+	interface INode {
 		Id: number;
 		ParentId: number | null;
 		Label: string;
+		Url: string | null;
 	}
-	interface ITidyTree {
-		Node: ITidyTreeData[];
-	}
-	export let controller: OutputController<ITidyTree>;
-	let svgContainer: HTMLElement | null = null;
-	let nullContainer: HTMLElement | null = null;
+
+	export let controller: OutputController<ITree>;
+
+	let div: HTMLElement;
 
 	let component = new OutputComponent({
 		refresh() {
 			if (controller.value) {
-				renderTree(controller.value.Node);
-			} else {
-				if (nullContainer) {
-					nullContainer.innerHTML = 'Data not found';
+				let chart = Tree(controller.value.Nodes, {
+					width: div.clientWidth - 100
+				});
+
+				if (chart != null) {
+					div.appendChild(chart);
 				}
 			}
 		}
@@ -30,88 +35,131 @@
 
 	beforeUpdate(async () => await component.setup(controller));
 
-	function renderTree(items: ITidyTreeData[]) {
-		let nodeMap = new Map();
-		let superParent: ITidyTreeData & { children: ITidyTreeData[] } = {
-			Id: -1,
-			Label: 'Node',
-			ParentId: null,
-			children: []
-		};
-		items.forEach((item) => {
-			nodeMap.set(item.Id, { ...item, children: [] });
-			let node = nodeMap.get(item.Id);
-			if (item.ParentId === null) {
-				superParent.children.push(node);
-			} else {
-				let parent = nodeMap.get(item.ParentId);
-				if (parent) {
-					parent.children.push(node);
-				}
-			}
+	function Tree(
+		data: INode[],
+		{
+			width = 800,
+			height,
+			r = 3, // radius of nodes
+			padding = 1, // horizontal padding for first and last column
+			fill = '#999', // fill for nodes
+			stroke = '#555' // stroke for links
+		}: {
+			width?: number;
+			height?: number;
+			r?: number;
+			padding?: number;
+			fill?: string;
+			stroke?: string;
+		} = {}
+	) {
+		// Build hierarchical tree (aka stratify).
+		const root = d3
+			.stratify<INode>()
+			.id((t) => t.Id.toString())
+			.parentId((t) => t.ParentId?.toString())(data);
+
+		// Compute labels and titles.
+		const descendants = root.descendants();
+		const L = descendants.map((d) => d.data.Label);
+
+		// Compute the layout.
+		const dx = 15;
+		const dy = width / (root.height + padding);
+		d3.tree<INode>().nodeSize([dx, dy])(root);
+
+		const rootPoints = root as unknown as d3.HierarchyPointNode<INode>;
+
+		// Center the tree.
+		let x0 = Infinity;
+		let x1 = -x0;
+		rootPoints.each((d) => {
+			if (d.x > x1) x1 = d.x;
+			if (d.x < x0) x0 = d.x;
 		});
 
-		let root = d3.hierarchy(superParent) as d3.HierarchyNode<unknown>;
-		let treeLayout = d3.tree().size([500, 500]);
-		treeLayout(root);
+		// Compute the default height.
+		if (height === undefined) height = x1 - x0 + dx * 2;
 
-		// Fit letters to image; up to 15 chars
-		let svg = d3.select(svgContainer).append('svg').attr('width', 900).attr('height', 600);
+		// Use the required curve
+		const svg = d3
+			.create('svg')
+			.attr('viewBox', [(-dy * padding) / 2, x0 - dx, width, height])
+			.attr('width', width)
+			.attr('height', height)
+			.attr('style', 'max-width: 100%; height: auto; height: intrinsic;')
+			.attr('font-family', 'sans-serif')
+			.attr('font-size', 10);
 
-		let g = svg.append('g').attr('transform', 'translate(50,50)');
-		let curve = function (d: d3.HierarchyLink<unknown>) {
-			let source = d.source as any;
-			let target = d.target as any;
-			return `M${source.y},${source.x} L${target.y},${target.x}`;
-		};
-
-		g.selectAll('.link')
-			.data(root.links())
-			.enter()
-			.append('path')
-			.style('stroke', 'grey')
-			.style('stroke-width', '1.5px')
-			.attr('d', curve);
-
-		let node = g
-			.selectAll('.node')
-			.data(root.descendants())
-			.enter()
+		svg
 			.append('g')
-			.attr('class', (d) => {
-				return 'node' + (d.children ? ' node--internal' : ' node--leaf');
-			})
-			.attr('transform', function (d) {
-				let node = d as any;
-				return 'translate(' + node.y + ',' + node.x + ')';
-			});
+			.attr('fill', 'none')
+			.attr('stroke', stroke)
+			.attr('stroke-opacity', 0.4)
+			.attr('stroke-width', 1.5)
+			.selectAll('path')
+			.data(rootPoints.links())
+			.join('path')
+			.attr(
+				'd',
+				d3
+					.link<any, d3.HierarchyPointNode<INode>>(d3.curveBumpX)
+					.x((d) => d.y)
+					.y((d) => d.x)
+			);
 
-		node.append('circle').attr('r', 5);
+		const node = svg
+			.append('g')
+			.selectAll('a')
+			.data(rootPoints.descendants())
+			.join('a')
+			.attr('xlink:href', (d) => d.data.Url)
+			.attr('target', (d) => (d.data.Url != null ? '_self' : null))
+			.attr('transform', (d) => `translate(${d.y},${d.x})`);
 
 		node
-			.append('text')
-			.attr('dx', '5px')
-			.attr('x', function (d) {
-				return d.children ? -10 : 10;
-			})
-			.style('text-anchor', (d) => (d.children ? 'end' : 'start'))
-			.text(function (d: d3.HierarchyNode<unknown>) {
-				let data = d.data as ITidyTreeData;
-				return data.Label.length > 15 ? data.Label.substring(0, 15) + '...' : data.Label;
-			});
+			.append('circle')
+			.attr('fill', (d) => (d.children ? stroke : fill))
+			.attr('r', r);
+
+		// Set tooltip.
+		node.append('title').text((n) =>
+			n
+				.ancestors()
+				.reverse()
+				.map((d) => d.data.Label)
+				.join(' / ')
+		);
+
+		if (L) {
+			node
+				.append('text')
+				.attr('dy', '0.32em')
+				.attr('x', (d) => (d.children ? -6 : 6))
+				.attr('text-anchor', (d) => (d.children ? 'end' : 'start'))
+				.attr('paint-order', 'stroke')
+				.attr('stroke', '#fff')
+				.attr('stroke-width', 3)
+				.text((d, i) => L[i]);
+		}
+
+		return svg.node();
 	}
 </script>
 
-<div bind:this={svgContainer} />
-<div bind:this={nullContainer} />
+<div bind:this={div} />
 
 <style>
-	.node circle {
+	/* .node circle {
 		fill: #000000;
 		stroke-width: 1px;
 	}
 	.node text {
 		font: 12px sans-serif;
 		font-weight: bold;
+	} */
+
+	div :global(svg a) {
+		text-decoration: none;
 	}
 </style>
