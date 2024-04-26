4188d6cbb4329c20a310754babdd42ab0e855d86
Jean <jeanlaurentc@unops.org>
2024-04-26 08:08:30 +0700

    feat(ConversationTimeline): improve colors + show read/unread

--- src/lib/Outputs/ConversationTimeline/ConversationCollapsibleSection.svelte
+++ src/lib/Outputs/ConversationTimeline/ConversationCollapsibleSection.svelte
@@ -2,9 +2,10 @@
 	export let headerText: string;
 	export let headerContent: string;
 	export let date: string;
-	export let status: string;
+	export let isRead: boolean;
+	export let isExternal: boolean;
 	export let icon: string;
-	export let style: string;
+	export let color: string;
 	export let isLast: boolean;
 
 	let expanded = false;
@@ -21,12 +22,12 @@
 <div class="outer-container">
 	<div class="timeline-container">
 		<div class="status-date-time-container">
-			<div class="status">{status}</div>
+			<div class="status">Sent on</div>
 			<div class="date-time">{myDate} - {myTime}</div>
 		</div>
 		<div class="icon-container">
-			<div class="icon-bg">
-				<i class={icon} {style} />
+			<div class="icon-bg" style:color>
+				<i class={icon} />
 			</div>
 
 			<div class={isLast ? 'vertical-line-last' : 'vertical-line'} />
@@ -37,9 +38,18 @@
 				<button
 					aria-expanded={expanded}
 					class="speech-bubble {expanded ? 'rounded-bottom' : ''}"
+					style:background-color={color}
+					style="--before-color: {color}"
 					on:click={() => (expanded = !expanded)}
 				>
 					<div class="header-container">
+						{#if isExternal}
+							{#if isRead}
+								<i class="fa-solid fa-envelope-open-text fa-xl" />
+							{:else}
+								<i class="fa-solid fa-envelope fa-xl" />
+							{/if}{/if}
+
 						<div class="header-text">{headerText}</div>
 						{#if !expanded}<div class="shorten-text">{@html ellipse(headerContent)}</div>{/if}
 					</div>
@@ -56,115 +66,116 @@
 		</div>
 	</div>
 </div>
+
 <style lang="scss">
-$primary-color: #56bbe8;
-$secondary-color: #f3f3f3;
-$tertiary-color: #cccccc;
+	$primary-color: #cccccc;
+	$secondary-color: #f3f3f3;
+	$tertiary-color: #cccccc;
 
-.header-container {
-    display: flex;
-    flex-direction: row;
-    align-items: baseline;
-}
+	.header-container {
+		display: flex;
+		flex-direction: row;
+		align-items: baseline;
+	}
 
-.header-text {
-    font-size: medium;
-}
+	.header-text {
+		font-size: medium;
+		padding-left: 10px;
+	}
 
-.shorten-text {
-    font-style: italic;
-    color: $secondary-color;
-    padding: 0px 0px 0px 15px;
-    font-size: larger;
-}
+	.shorten-text {
+		font-style: italic;
+		color: $secondary-color;
+		padding: 0px 0px 0px 15px;
+		font-size: larger;
+	}
 
-.outer-container {
-    display: flex;
-}
+	.outer-container {
+		display: flex;
+	}
 
-.timeline-container {
-    display: flex;
-    gap: 20px;
-    align-items: flex-start;
-    flex-grow: 1;
-}
+	.timeline-container {
+		display: flex;
+		gap: 20px;
+		align-items: flex-start;
+		flex-grow: 1;
+	}
 
-.status-date-time-container {
-    display: flex;
-    flex-direction: column;
-    font-size: 9px;
-    width: 12%;
-    margin-top: 5px;
-}
+	.status-date-time-container {
+		display: flex;
+		flex-direction: column;
+		font-size: 9px;
+		width: 12%;
+		margin-top: 5px;
+	}
 
-.status {
-    color: $tertiary-color;
-}
+	.status {
+		color: $tertiary-color;
+	}
 
-.date-time {
-    font-size: small;
-}
+	.date-time {
+		font-size: small;
+	}
 
-.icon-container {
-    margin-top: 9px;
-    align-items: center;
-    height: 100%;
-}
+	.icon-container {
+		margin-top: 9px;
+		align-items: center;
+		height: 100%;
+	}
 
-.collapsible-container {
-    flex: 1;
-    margin-bottom: 5px;
-}
+	.collapsible-container {
+		flex: 1;
+		margin-bottom: 5px;
+	}
 
-.icon-bg {
-    background-color: white;
-}
+	.icon-bg {
+		background-color: white;
+	}
 
-.vertical-line {
-    border-left: 3px solid $primary-color;
-    margin-left: 50%;
-    height: 100%;
-}
+	.vertical-line {
+		border-left: 3px solid $primary-color;
+		margin-left: 50%;
+		height: 100%;
+	}
 
-.vertical-line-last {
-    border-left: 3px dashed $primary-color;
-    margin-left: 50%;
-    height: 50%;
-}
+	.vertical-line-last {
+		border-left: 3px dashed $primary-color;
+		margin-left: 50%;
+		height: 50%;
+	}
 
-.speech-bubble {
-    position: relative;
-    padding: 10px;
-    background-color: $primary-color;
-    border-radius: 10px;
-    color: white;
-    cursor: pointer;
+	.speech-bubble {
+		position: relative;
+		padding: 10px;
+		border-radius: 10px;
+		color: white;
+		cursor: pointer;
 
-    &::before {
-        content: '';
-        position: absolute;
-        top: 50%;
-        right: 100%;
-        margin-top: -10px;
-        width: 0;
-        height: 0;
-        border-top: 10px solid transparent;
-        border-right: 10px solid $primary-color;
-        border-bottom: 10px solid transparent;
-    }
-}
+		&::before {
+			content: '';
+			position: absolute;
+			top: 50%;
+			right: 100%;
+			margin-top: -10px;
+			width: 0;
+			height: 0;
+			border-top: 10px solid transparent;
+			border-right: 10px solid var(--before-color);
+			border-bottom: 10px solid transparent;
+		}
+	}
 
-.rounded-bottom {
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-}
+	.rounded-bottom {
+		border-bottom-left-radius: 0;
+		border-bottom-right-radius: 0;
+	}
 
-.collapsible-container button {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    background: $primary-color;
-    border: none;
-}
+	.collapsible-container button {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		background: $primary-color;
+		border: none;
+	}
 </style>
--- src/lib/Outputs/ConversationTimeline/ConversationTimeline.svelte
+++ src/lib/Outputs/ConversationTimeline/ConversationTimeline.svelte
@@ -8,9 +8,10 @@
 		Label: string;
 		Date: DateTime;
 		Content: string;
-		Status: string;
+		IsRead: boolean;
+		IsExternal: boolean;
 		Icon: string;
-		Style: string;
+		Color: string;
 		Actions: ActionListData | null;
 	}
 
@@ -52,6 +53,24 @@
 		const regex = /(width:[^;]+;?)|(\s+width:[^;]+;?)/g;
 		return Content.replace(regex, '');
 	}
+
+	function AddOpacity(hex: string | null) {
+		if (!hex) {
+			return 'gray';
+		}
+
+		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		var colorArray = result
+			? {
+					r: parseInt(result[1], 16),
+					g: parseInt(result[2], 16),
+					b: parseInt(result[3], 16),
+					a: 0.3
+			  }
+			: { r: 10, g: 20, b: 30, a: 0.3 };
+
+		return `rgba(${colorArray.r}, ${colorArray.g}, ${colorArray.b}, ${colorArray.a})`;
+	}
 </script>
 
 {#if controller.value != null}
@@ -60,12 +79,13 @@
 			headerContent={item.Content}
 			headerText={item.Label}
 			date={item.Date}
-			status={item.Status}
+			isRead={item.IsRead}
+			isExternal={item.IsExternal}
 			icon={item.Icon}
-			style={item.Style}
+			color={item.Color}
 			isLast={index == controller.value?.Items?.length - 1}
 		>
-			<div class="collapsible">
+			<div class="collapsible" style:background-color={AddOpacity(item.Color)}>
 				<div class="collapsible-content">
 					{@html RemoveWidthStyling(item.Content)}
 				</div>
@@ -81,11 +101,10 @@
 	@import '../../scss/styles.variables.scss';
 	.collapsible {
 		padding: 0px 3px 3px;
-		background-color: #7fd0ff;
 		border-radius: 0px 0px 5px 5px;
 	}
+
 	.collapsible-content {
-		background-color: #dff2ff;
 		border-width: 0;
 		padding: 15px;
 		text-align: left;
