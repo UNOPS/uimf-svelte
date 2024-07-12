UNOPS/uimf-svelte

commit 1fa412c9639621833001c611bb15f8f3166aff1d

feat(ValueList): css

src/lib/Inputs/ValueList.svelte
@@ -371,6 +371,7 @@
 		--inner-border-color: rgba(0, 0, 0, 0.03);
 		--outer-border-color: #ebebeb;
 		--group-border-color: #d6d6d645;
+		min-width: min-content;
 
 		thead {
 			& > tr:last-child {
@@ -427,11 +428,10 @@
 			padding-left: 8px;
 			padding-right: 8px;
 		}
-
-		td.min-width-200 {
-			min-width: 200px;
-		}
 	}
+
+
+.table-data > tbody > tr > 
 
 	.has-dropdowns {
 		// Add padding to account for typeahead, dropdown or any other input
@@ -442,5 +442,10 @@
 	.col-action {
 		width: 1px;
 		text-align: center;
+	}
+
+	.min-width-400{
+		min-width: 400px;
+		display: flex;
 	}
 </style>

commit be7a6afa12c618c895a65d088cb946fd7b8a32fb

fix(value-list): revert to correct CSS styles, maintain new style
This fixes the issues introduced by commit 1fa412c9639621833001c611bb15f8f3166aff1d.

src/lib/Inputs/ValueList.svelte
@@ -447,9 +447,18 @@
 			padding-left: 8px;
 			padding-right: 8px;
 		}
+
+		td.min-width-200 {
+			min-width: 200px;
+		}
+
+		td.min-width-400 {
+			min-width: 400px;
+			display: flex;
+		}
 	}
 
-	.table-data > tbody > tr > .has-dropdowns {
+	.has-dropdowns {
 		// Add padding to account for typeahead, dropdown or any other input
 		// that can grow vertically.
 		padding-bottom: 18rem;
@@ -458,11 +467,6 @@
 	.col-action {
 		width: 1px;
 		text-align: center;
-	}
-
-	.min-width-400 {
-		min-width: 400px;
-		display: flex;
 	}
 
 	button.btn {