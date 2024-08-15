f6f44e646605ed51b7243f9c71ae05660b2e101b
Nik <418400+codingawayy@users.noreply.github.com>
2024-08-15 13:34:53 +0700

    fix(tooltip)

--- src/lib/Components/Tooltip.svelte
+++ src/lib/Components/Tooltip.svelte
@@ -1,25 +1,27 @@
 <script context="module" lang="ts">
-	import tippy from 'sveltejs-tippy';
+	import tippy, { roundArrow } from 'tippy.js';
+	import 'tippy.js/dist/svg-arrow.css';
 
 	let tooltips = new Map();
 
 	function process(node: HTMLElement, documentation: any) {
-		if (documentation != null) {
-			var content = typeof documentation === 'string' ? documentation : documentation.Content;
+		let content = typeof documentation === 'string' ? documentation : documentation?.Content;
 
-			if (content == null || content.length === 0) {
-				return;
-			}
+		if (content != null && content != '') {
+			let instance = tooltips.get(node);
 
-			if (tooltips.get(node) != null) {
+			if (instance != null) {
 				// Re-use existing instance, but just change the content.
-				tooltips.get(node).setContent(content);
+				instance.setContent(content);
 			} else {
 				node.classList.add('has-documentation');
 
-				const instance = tippy(node, {
+				instance = tippy(node, {
 					content: content,
-					allowHTML: true
+					allowHTML: true,
+					delay: [100, 0],
+					duration: [0, 0],
+					arrow: roundArrow
 				});
 
 				tooltips.set(node, instance);
@@ -27,7 +29,8 @@
 		} else {
 			node.classList.remove('has-documentation');
 
-			var instance = tooltips.get(node);
+			let instance = tooltips.get(node);
+
 			if (instance != null) {
 				instance.destroy();
 				tooltips.delete(node);
@@ -39,7 +42,14 @@
 		process(node, documentation);
 
 		return {
-			destroy() {},
+			destroy() {
+				const instance = tooltips.get(node);
+
+				if (instance != null) {
+					instance.destroy();
+					tooltips.delete(node);
+				}
+			},
 			update(newDocumentation: any) {
 				process(node, newDocumentation);
 			}
@@ -51,10 +61,15 @@
 	@import '../scss/styles.variables.scss';
 
 	:global(.tippy-box) {
+		--bg-color: #313131;
+
 		font-size: $font-size-base !important;
 		line-height: 1.4em !important;
 		border-radius: 2px !important;
 		text-align: center;
+		background: var(--bg-color);
+		color: #f6f6f6;
+		padding: 2px 5px;
 
 		ul {
 			text-align: left;
@@ -62,5 +77,9 @@
 			margin: 8px 0;
 			list-style-position: outside;
 		}
+	}
+
+	:global(.tippy-box > .tippy-svg-arrow) {
+		fill: var(--bg-color);
 	}
 </style>
