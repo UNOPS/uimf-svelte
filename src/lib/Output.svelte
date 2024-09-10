9fcf1a49dfd3f3156b6c9079a4ac63c7c873e1d9
Nik <418400+codingawayy@users.noreply.github.com>
2024-09-10 18:18:06 +0700

    refactor: remove unused prop

--- src/lib/Output.svelte
+++ src/lib/Output.svelte
@@ -8,7 +8,6 @@
 
 	export let controller: OutputController<any>;
 	export let hideLabel: boolean = false;
-	export let contentCssClass: string | null = null;
 	export let nolayout: boolean = false;
 
 	// Field constants.
@@ -74,7 +73,6 @@
 				>
 			{/if}
 			<div
-				class="{contentCssClass ?? ''} {controller.metadata.CustomProperties?.cssClassLabel ?? ''}"
 				class:col-sm-10={horizontalLayout && !thisHideLabel}
 				class:col-sm-12={horizontalLayout && thisHideLabel}
 			>
