e4ffdf3484979c873cc5a4610f64a026db4f4e71
Nik <418400+codingawayy@users.noreply.github.com>
2024-09-13 17:14:57 +0700

    chore: remove demo pages

--- src/routes/demo/inputs/richtexteditor/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Editor from '$lib/Inputs/RichTextEditor.svelte';
-	
-    let editor;
-</script>
-
-<h1>Inputs / Text</h1>
-<Editor bind:this={editor} />
-<button on:click={() => alert(editor.getHtml()) } >Get html</button>
