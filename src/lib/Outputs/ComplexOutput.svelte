ff25c7b06f1721f44cabeb98b1f3e19023e1130e
Nik <418400+codingawayy@users.noreply.github.com>
2025-03-24 14:57:07 +0700

    feat(complex-output): areas

--- src/lib/Outputs/ComplexOutput.svelte
+++ src/lib/Outputs/ComplexOutput.svelte
@@ -5,23 +5,42 @@
 	import type { IOutputFieldMetadata } from '$lib/Infrastructure/uimf';
 	import Output from '../Output.svelte';
 
+	type Group<Tkey extends string | number | symbol, TItem> = { Key: Tkey; Items: TItem[] };
+
+	interface IComplexOutputFieldMetadata extends IOutputFieldMetadata {
+		CustomProperties?: {
+			ComplexOutputItem?: {
+				Area: string;
+			};
+		};
+	}
+
+	interface IArea {
+		Name: string | null;
+		CssClass: string | null;
+		OrderIndex: number;
+	}
+
 	interface Configuration {
 		CssClass: string;
-		Properties: IOutputFieldMetadata[];
+		Properties: IComplexOutputFieldMetadata[];
+		Areas: IArea[] | null;
 	}
+
+	type NestedField = OutputController<any, IComplexOutputFieldMetadata>;
 
 	class Controller extends OutputController<any, IOutputFieldMetadata<Configuration>> {}
 
 	export let controller: Controller;
 
-	let fields: OutputController<any>[] = [];
+	let areas: { Area: IArea | null; Items: NestedField[] }[] = [];
 
 	let component = new OutputComponent({
 		refresh() {
-			fields = controller.metadata.Component.Configuration.Properties.filter((t) => !t.Hidden)
+			let fields = controller.metadata.Component.Configuration.Properties.filter((t) => !t.Hidden)
 				.sort((a, b) => a.OrderIndex - b.OrderIndex)
 				.map((property) => {
-					return new OutputController<any>({
+					return new OutputController<any, IComplexOutputFieldMetadata>({
 						metadata: property,
 						data: controller.value == null ? null : controller.value[property.Id],
 						form: controller.form,
@@ -29,6 +48,36 @@
 						parent: controller
 					});
 				});
+
+			let results: Record<string, { Area: IArea | null; Fields: NestedField[] }> = {};
+
+			for (let i = 0; i < fields.length; i++) {
+				const field = fields[i];
+				let areaName = field.metadata.CustomProperties?.ComplexOutputItem?.Area ?? null;
+
+				// Make sure key is not null.
+				const key = areaName ?? '';
+
+				if (!results[key]) {
+					let area =
+						controller.metadata.Component.Configuration.Areas?.find((t) => t.Name == areaName) ??
+						null;
+
+					results[key] = {
+						Area: area,
+						Fields: []
+					};
+				}
+
+				results[key].Fields.push(field);
+			}
+
+			// Sort `results` entries by `Area.OrderIndex` and put in array.
+			areas = Object.entries(results)
+				.map(([_, value]) => {
+					return { Area: value.Area, Items: value.Fields };
+				})
+				.sort((a, b) => (a.Area?.OrderIndex ?? 0) - (b.Area?.OrderIndex ?? 0));
 		}
 	});
 
@@ -36,8 +85,12 @@
 </script>
 
 <div class={controller.metadata.Component.Configuration?.CssClass} class:complex-output={true}>
-	{#each fields as field}
-		<Output controller={field} />
+	{#each areas as area}
+		<div class={area.Area?.CssClass}>
+			{#each area.Items as field}
+				<Output controller={field} />
+			{/each}
+		</div>
 	{/each}
 </div>
 
