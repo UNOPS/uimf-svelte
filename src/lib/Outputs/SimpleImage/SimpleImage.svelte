8cb2a77c729248fd2fe651944a6cd8ea7a507402
nik <418400+codingawayy@users.noreply.github.com>
2025-10-03 20:19:30 +0700

    feature/wip

--- src/lib/Outputs/SimpleImage/SimpleImage.svelte
+++ src/lib/Outputs/SimpleImage/SimpleImage.svelte
@@ -1,10 +1,16 @@
 <script lang="ts" context="module">
+	interface SimpleImageLink {
+		Form: string | null;
+		InputFieldValues: Record<string, any> | null;
+	}
+
 	interface SimpleImage {
 		Src: string;
 		Alt: string | null;
 		Width: string | null;
 		Height: string | null;
 		CssClass: string | null;
+		Link: SimpleImageLink | null;
 	}
 </script>
 
@@ -22,11 +28,25 @@
 </script>
 
 {#if controller.value?.Src}
-	<img
-		src={controller.value.Src}
-		alt={controller.value?.Alt || ''}
-		width={controller.value?.Width || undefined}
-		height={controller.value?.Height || undefined}
-		class={controller.value?.CssClass || ''}
-	/>
+	{#if controller.value.Link?.Form}
+		{#await controller.app.makeUrl({ Form: controller.value.Link.Form, InputFieldValues: controller.value.Link.InputFieldValues || {}, Action: 'redirect' }) then url}
+			<a href={url}>
+				<img
+					src={controller.value.Src}
+					alt={controller.value?.Alt || ''}
+					width={controller.value?.Width || undefined}
+					height={controller.value?.Height || undefined}
+					class={controller.value?.CssClass || ''}
+				/>
+			</a>
+		{/await}
+	{:else}
+		<img
+			src={controller.value.Src}
+			alt={controller.value?.Alt || ''}
+			width={controller.value?.Width || undefined}
+			height={controller.value?.Height || undefined}
+			class={controller.value?.CssClass || ''}
+		/>
+	{/if}
 {/if}
