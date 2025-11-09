8520bf37e83239c9b6e9129529b02e8978952ac2
takara <418400+codingawayy@users.noreply.github.com>
2025-11-09 13:50:25 +0700

    refactor: cleanup

--- src/lib/Infrastructure/Metadata/IOutputFieldMetadata.ts
+++ src/lib/Infrastructure/Metadata/IOutputFieldMetadata.ts
@@ -1,5 +1,4 @@
 import type { IFieldMetadata } from "./IFieldMetadata";
-import type { FieldLayout } from "./FieldLayout";
 
 export interface IOutputFieldMetadata<TConfiguration = any> extends IFieldMetadata<TConfiguration> {
     HideIfNull: boolean;
