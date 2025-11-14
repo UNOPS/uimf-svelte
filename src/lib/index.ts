622c4edf2af7d364d6dcc999fa195038ecb0d862
nik <418400+codingawayy@users.noreply.github.com>
2025-11-14 12:08:17 +0700

    refactor: give property type to `window.SvelteComponents`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,25 @@
 // Reexport your entry components here
-import { defaultControlRegister } from "./Infrastructure/ControlRegister";
+import { defaultControlRegister, type ControlRegister } from "./Infrastructure/ControlRegister";
 import AppStorage from "./Infrastructure/Storage/App/AppStorage";
+import type { SvelteComponent } from "svelte";
 import SmartNavigator from './Components/SmartNavigator.svelte';
 import './scss/styles.scss';
 import { UimfApp } from "./Infrastructure/App/UimfApp";
 import { ClientFunctionRegistry } from "./Infrastructure/ClientFunctions/ClientFunctionRegistry";
 import { Loader } from "./Components/Loader/Loader";
 
+export interface ISvelteComponents {
+	SmartNavigator: typeof SvelteComponent<any>;
+	AppStorage: typeof AppStorage;
+	controlRegister: ControlRegister;
+	UimfApp: typeof UimfApp;
+	ClientFunctionRegistry: Record<string, any>;
+	loader: Loader;
+}
+
 declare global {
     interface Window {
-        SvelteComponents: any;
+        SvelteComponents: ISvelteComponents;
     }
 }
 
